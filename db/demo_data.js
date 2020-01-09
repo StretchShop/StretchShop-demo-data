dbe3605fc8e7ba8c838583e5635f8bd6e654c1f2
Marcel Zúbrik <wradgio@gmail.com>
2020-01-09 19:59:06 +0100

    Fixing the date format

--- db/demo_data.js
+++ db/demo_data.js
@@ -26,6 +26,20 @@ MongoClient.connect(url, function(err, database) {
 				let extension = path.extname(filename);
 				let purename = path.basename(filename, extension);
 				console.log("importing collection:", purename);	
+				// if in date collection and has records
+				let dateCollections = ["users", "pages"];
+				if ( dateCollections.indexOf(purename) && documents && documents.length>0 ) { 
+					for (let i=0; i<documents.length; i++) {
+						if ( documents[i].createdAt ) { documents[i].createdAt = new Date(documents[i].createdAt); }
+						if ( documents[i].lastVerifyDate ) { documents[i].lastVerifyDate = new Date(documents[i].lastVerifyDate); }
+						if ( documents[i].activated ) { documents[i].activated = new Date(documents[i].activated); }
+						if ( documents[i].dates ) { 
+							if ( documents[i].dates.dateCreated ) { documents[i].dates.dateCreated = new Date(documents[i].dates.dateCreated); }
+							if ( documents[i].dates.dateUpdated ) { documents[i].dates.dateUpdated = new Date(documents[i].dates.dateUpdated); }
+							if ( documents[i].dates.dateSynced ) { documents[i].dates.dateSynced = new Date(documents[i].dates.dateSynced); }
+						 }
+					}
+				}
 				let collection = db.collection(purename);
 				importer.push(collection.insertMany(documents),{w:0,ordered:false});
 			});
